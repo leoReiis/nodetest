f69934d4342d008ddf6722006339a16e946e9710
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2024-03-20 20:53:05 -0300

    projeto basico node

---
+++ database-memory.js
@@ -0,0 +1,34 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+   #videos = new Map()
+
+   //Set   set é como se fosse um array que não aceita valores duplicados
+   //Map como se fosse um objeto com suas particularidades
+   
+    list(){
+       return Array.from(this.#videos.entries()).map((videoArray) => {
+        const id = videoArray[0]
+        const data = videoArray[1]
+
+        return {
+            id, 
+            ...data,
+        }
+       })
+    }
+
+   create(video){
+    const videoId = randomUUID()
+    this.#videos.set(videoId, video)
+   }
+
+   update(id, video)
+   {
+    this.#videos.set(id, video)
+   }
+
+   delete(){
+    this.#videos.delete(id)
+   }
+}
